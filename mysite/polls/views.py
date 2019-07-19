1ca211e346ccabd4e3d42dc8d1cf7589c7e31b4b
yuanyaru <398916756@qq.com>
2019-07-19 10:02:17 +0800

    创建公用界面(视图)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, World, You're at the polls index.")
+    # 展示数据库里以发布日期排序的最近 5 个投票问题，以空格分割
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    # 载入模板
+    # template = loader.get_template('polls/index.html')
+    # 填充上下文
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # output = ','.join([q.question_text for q in latest_question_list])
+    # return HttpResponse("Hello, World, You're at the polls index.")
+    # return HttpResponse(output)
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
+# 投票详情视图
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exit")
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    # return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
