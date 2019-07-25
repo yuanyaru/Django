8053c3a83b2b6511f68afefcae67082248b20b0c
yuanyaru <398916756@qq.com>
2019-07-25 15:55:17 +0800

    自定义后台表单

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date information', {'fields': ['pub_date']})]
+
+
+# 修改模型的后台管理选项
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
