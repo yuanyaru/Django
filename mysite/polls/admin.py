97a1c475dfa97da8eab6f882a619555d931eae44
yuanyaru <398916756@qq.com>
2019-07-25 17:09:48 +0800

    自定义你的 工程的 模板

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,6 +12,9 @@ class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
     fieldsets = [(None, {'fields': ['question_text']}), ('Date information', {'fields': ['pub_date']})]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 # 修改模型的后台管理选项
