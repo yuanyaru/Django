80101f6e3fe1b53fecc217480e258b64e1d46bde
yuanyaru <398916756@qq.com>
2019-07-25 16:21:00 +0800

    添加关联的对象

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
     fieldsets = [(None, {'fields': ['question_text']}), ('Date information', {'fields': ['pub_date']})]
+    inlines = [ChoiceInline]
 
 
 # 修改模型的后台管理选项
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+# admin.site.register(Choice)
